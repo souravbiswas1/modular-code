2b8ca700cf0a24e376c029b0591e4c4ceefb0d07
Sourav Biswas (CW) <Sourav.Biswas@jda.com>
2021-09-19 23:15:15 +0530

    logging & exception handled

--- or.py
+++ or.py
@@ -1,10 +1,17 @@
 from utils.model import Perceptron
 from utils.all_utils import prepare_data,save_model,save_plot
 import pandas as pd
+import logging
+import os
+
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s] %(message)s"
+log_dir = "logs"
+os.makedirs(log_dir,exist_ok=True)
+logging.basicConfig(filename=os.path.join(log_dir,"running_logs.log"),level=logging.INFO, format=logging_str)
 
 def main(data, eta, epoch, filename, plotFileName):
     df = pd.DataFrame(data)
-    print(df)
+    logging.info(f"This is pandas dataframe {df}")
 
     X,y = prepare_data(df)
 
@@ -26,4 +33,10 @@ if __name__ == '__main__':
     ETA = 0.3 # 0 and 1
     EPOCHS = 10
 
-    main(data=OR, eta=ETA, epoch=EPOCHS, filename='or.model', plotFileName='or.png')
+    try:
+        logging.info(">>>>>> Starting training >>>>>>>")
+        main(data=OR, eta=ETA, epoch=EPOCHS, filename='or.model', plotFileName='or.png')
+        logging.info("<<<<<< Training completed successfully <<<<<<<")
+    except Exception as e:
+        logging.info(e)
+        raise e
